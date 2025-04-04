302bd69174fcf7f837c41b22b830db1103ec5531
Suraj Shrivastava <srjshrivastav1@gmail.com>
2025-04-05 01:49:36 +0530

    Added Number Right triangle

--- patterns/RightTriangle.java
+++ patterns/RightTriangle.java
@@ -7,12 +7,23 @@ public class RightTriangle {
     public static void main(String[] args) {
         int n = Helper.getIntergerInput();
         printTriangle(n);
+        System.out.println("\n");
+        printNumberTiangle(n);
     }
 
     private static void printTriangle(int n) {
         for(int i=1;i<=n;i++){
             for(int j=0;j<i;j++){
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printNumberTiangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(j+1);
             }
             System.out.println();
         }
