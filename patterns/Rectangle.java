0efcf96dc59aac3e81b893ad7902d752f734b9b0
Suraj Shrivastava <srjshrivastav1@gmail.com>
2025-04-07 00:02:28 +0530

    Added Hollow Rectangle

--- patterns/Rectangle.java
+++ patterns/Rectangle.java
@@ -7,6 +7,27 @@ public class Rectangle {
     public static void main(String[] args) {
         int n = Helper.getIntergerInput();
         printRectangle(n);
+        System.out.println("\n");
+        printRectangleWithSpaceBetween(n);
+    }
+
+    private static void printRectangleWithSpaceBetween(int n) {
+        StringBuilder sb=new StringBuilder();
+        for(int i= 0;i<(n*2)-1;i++){
+            sb.setLength(0);
+            for(int j=0;j<n;j++){
+                if(i%2 != 0){
+                    sb.append(" ");
+                }else{
+                    if(i==0 || i==(n*2)-2 || j==0 || j == n-1){
+                        sb.append("*");
+                    }else{
+                        sb.append(" ");
+                    }
+                }
+            }
+            System.out.println(sb.toString());
+        }
     }
 
     private static void printRectangle(int n) {
