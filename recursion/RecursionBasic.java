5366cc9f97a0eedd555889e3a2954d8e7d0c55b7
Suraj Shrivastava <srjshrivastav1@gmail.com>
2025-04-08 02:17:44 +0530

    Added factorial resursive

--- recursion/RecursionBasic.java
+++ recursion/RecursionBasic.java
@@ -8,6 +8,17 @@ public class RecursionBasic {
         printNTimes(n, "Suraj");
         System.out.println();
         print1ToN(1, n);
+        System.out.println();
+        printNTo1(n);
+        sumOfN(0, n);
+        System.out.println("Factorial of "+n +" is: "+ fact(n));
+    }
+
+    private static int fact(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        return n * fact(--n);
     }
 
     private static void printNTimes(int n, String string) {
@@ -22,5 +33,22 @@ public class RecursionBasic {
         System.out.println(current);
         current ++;
         print1ToN(current, n);
+    }
+
+    private static void printNTo1(int n) {
+        if(n == 0) return;
+        System.out.println(n);
+        n--;
+        printNTo1(n);
+    }
+
+    private static void sumOfN(int sum, int n) {
+        if(n == 0) {
+            System.out.println("Sum of N numbers: " + sum);
+            return;
+        }
+        sum =  sum + n;
+        n--;
+        sumOfN(sum, n);
     }
 }
