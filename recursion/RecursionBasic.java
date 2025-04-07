023eac1fde5cbd9004a795592c5b9878a130bdec
Suraj Shrivastava <srjshrivastav1@gmail.com>
2025-04-08 01:58:11 +0530

    Added 1 to N print Recursion

--- recursion/RecursionBasic.java
+++ recursion/RecursionBasic.java
@@ -6,6 +6,8 @@ public class RecursionBasic {
     public static void main(String[] args) {
         int n = Helper.getIntergerInput();
         printNTimes(n, "Suraj");
+        System.out.println();
+        print1ToN(1, n);
     }
 
     private static void printNTimes(int n, String string) {
@@ -13,5 +15,12 @@ public class RecursionBasic {
         System.out.println(string);
         n--;
         printNTimes(n, string);
+    }
+
+    private static void print1ToN(int current, int n) {
+        if(current > n) return;
+        System.out.println(current);
+        current ++;
+        print1ToN(current, n);
     }
 }
