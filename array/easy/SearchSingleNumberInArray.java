274ccd5c1ed94a15f47ce9aa0955fd9e0897eb50
Suraj Shrivastava <srjshrivastav1@gmail.com>
2025-04-15 01:03:55 +0530

    Added XOR properties in the comments

--- array/easy/SearchSingleNumberInArray.java
+++ array/easy/SearchSingleNumberInArray.java
@@ -8,6 +8,15 @@ public class SearchSingleNumberInArray {
         System.out.println(findSingleNumberInArray(arr));
     }
 
+    /**
+     * XOR Properties
+     * XOR of Two same no. results 0 and XOR any Number with 0 results itself the number Ex.
+     * 
+     * 1 ^ 1 = 0, 5 ^ 5 = 0
+     * 0 ^ 1 = 1, 0 ^ 2 = 2
+     * @param arr
+     * @return
+     */
     private static int findSingleNumberInArray(int[] arr) {
         int xor = 0;
         for(int i:arr){
